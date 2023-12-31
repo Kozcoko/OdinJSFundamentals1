c4bb3faaa98d4d32974e1cf5a2b9f51b60b3b559
Kozcoko <116641538+Kozcoko@users.noreply.github.com>
2023-12-30 23:17:12 -0500

    Forgot to do git stuff til now.

---
+++ jsFundamentals1/javascript.js
@@ -0,0 +1,43 @@
+let user = 'Zelikaion'; 
+ age = '124,003'; 
+ message = 'Hello there!';
+
+let warning = "Oh no! You've a aquired a virus (;_;)! Your computer is now compromised and must be cleansed! Click OK to make this message bugger off!";
+
+
+let greeting = 'Greetings!';
+let hello;
+
+hello = greeting;
+
+
+const sverdFeathers = '20,000';
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_BLUE;
+// alert(color)
+
+console.log("By the Swords' See, it is seen.")
+
+// assignment:
+let admin, name;
+name = 'Zhykolarr';
+admin = name;
+
+alert(admin);
+//Complete, but why is 'name' deprecated?
+
+// assignment:
+let planetEarth = 'Earth';
+let presentUser; //Insert current user
+//Complete, but missing users.
+
+// assignment;
+const BIRTHDEDDAY = '7.03.2006';
+
+//const age = someCode(BIRTHDEDDAY);
+//Complete. Had to comment it out because someCode doesn't do code
